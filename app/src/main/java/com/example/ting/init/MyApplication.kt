7e8e39bd17e8421b3ced66bdc92e9f6629a7dcd0
Fan <fan1138612367@vip.qq.com>
2022-06-11 11:39:35 +0800

    Coil缓存

--- app/src/main/java/com/example/ting/init/MyApplication.kt
+++ app/src/main/java/com/example/ting/init/MyApplication.kt
@@ -1,7 +1,25 @@
 package com.example.ting.init
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application(), ImageLoaderFactory {
+    override fun newImageLoader() = ImageLoader.Builder(this)
+        .memoryCache {
+            MemoryCache.Builder(this)
+                .maxSizePercent(0.25)
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(cacheDir.resolve("image_cache"))
+                .maxSizePercent(0.02)
+                .build()
+        }
+        .build()
+}
